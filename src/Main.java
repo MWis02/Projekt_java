6af73a637c3a5d89bd9a6ae50ee6ca5f8f8b53b5
Marcin Wiśniowski <marcin.wisniowski@opoczta.pl>
2024-04-17 22:39:28 +0200

    Kolejne zmiany laoutu

--- src/Main.java
+++ src/Main.java
@@ -16,8 +16,11 @@ public class Main {
             UIManager.put("control", Color.decode("#EADFB4"));
 
             // Dostosowanie czcionki
-            UIManager.put("Label.font", new Font("Arial", Font.BOLD, 14));
-            UIManager.put("Button.font", new Font("Arial", Font.PLAIN, 12));
+            UIManager.put("Label.font", new Font("Calibri", Font.BOLD, 18));
+            UIManager.put("Button.font", new Font("Calibri Light", Font.PLAIN, 14));
+            UIManager.put("TextArea.font", new Font("Calibri Light", Font.PLAIN, 14));
+            UIManager.put("TextField.font", new Font("Calibri Light", Font.PLAIN, 14));
+            UIManager.put("JFrame.font", new Font("Calibri", Font.BOLD, 18));
 
             // Dostosowanie wyglądu JButton
             UIManager.put("Button.background", Color.decode("#9BB0C1"));
@@ -26,6 +29,10 @@ public class Main {
             // Dostosowanie wyglądu JTextArea
             UIManager.put("TextArea.background", Color.decode("#F1F1F2"));
             UIManager.put("TextArea.foreground", Color.black);
+
+            // Dostosowanie wyglądu JTextField
+            UIManager.put("TextField.background", Color.decode("#F1F1F2"));
+            UIManager.put("TextField.foreground", Color.black);
 
         } catch (UnsupportedLookAndFeelException e) {
             throw new RuntimeException(e);
